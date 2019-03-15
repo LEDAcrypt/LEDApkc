3a668324f1ba34ef3a4f95ee6e0398cb2128dce2
Daniel Norte de Moraes <danielcheagle@gmail.com>
2019-03-15 16:06:42 -0300

    Makefile: added targets: shared, install and uninstall. Files: added api_length.c to access compiled api nist values in libs. Enjoy!!

---
+++ Reference_Implementation/LEDApkc/library/api_length.c
@@ -0,0 +1,42 @@
+/**
+ *
+ * <api_length.c>
+ *
+ * @version 1.0 (September 2017)
+ *
+ * Reference ISO-C99 Implementation of LEDAkem cipher" using GCC built-ins.
+ *
+ * This file is necessary for access the original compiled values.
+ * Python script depend this file.
+ *
+ *
+ * In alphabetical order:
+ *
+ * @author Marco Baldi <m.baldi@univpm.it>
+ * @author Alessandro Barenghi <alessandro.barenghi@polimi.it>
+ * @author Franco Chiaraluce <f.chiaraluce@univpm.it>
+ * @author Gerardo Pelosi <gerardo.pelosi@polimi.it>
+ * @author Paolo Santini <p.santini@pm.univpm.it>
+ * @author Daniel Norte de Moraes <danielcheagle@gmail.com>
+ *
+ * This code is hereby placed in the public domain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ''AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **/
+
+#include "api.h"
+
+unsigned long long crypto_secretkeybytes = CRYPTO_SECRETKEYBYTES;
+unsigned long long crypto_publickeybytes = CRYPTO_PUBLICKEYBYTES;
+unsigned long long crypto_bytes = CRYPTO_BYTES;
